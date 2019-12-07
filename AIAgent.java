b2829bb9e22ac70cd35d92a2f5c213ce62b6de2b
Mark Redmond <markred123@gmail.com>
2019-12-07 14:00:54 +0000

    an understanding

--- AIAgent.java
+++ AIAgent.java
@@ -25,28 +25,41 @@ public class AIAgent{
   }
 
   public Move nextBestMove(Stack whitePossibilitiesStack, Stack blackPossibilitiesStack) {
+    //This stack has all the white possibilities
       Stack backupMove = (Stack) whitePossibilitiesStack.clone();
+      //this stack has the black positions
       Stack bStack = (Stack) blackPossibilitiesStack.clone();
+      //stores the best move selection
       Move bestMove = null;
+      //stores the selected white move
       Move whiteMove;
+      //stores the current move
       Move presentMove;
+      //stores the black position squares
       Square blackPosition;
+      // strength of the pieces for the second loop
       int strength = 0;
+      //the strength of the chosen piece
       int chosenPieceStrength = 0;
-
+// for every single white possibility the loop will cycle and try for every single possibility and check the result
       while (!whitePossibilitiesStack.empty()) {
+        //every move that's tried is removed from the stack
           whiteMove = (Move) whitePossibilitiesStack.pop();
+          //the move that's tried is selected as the present move
           presentMove = whiteMove;
 
 
 
-          //compare white landing positions to black positions, return capture if available or random if not.
+          // this loop cycles through the black positions
           while (!bStack.isEmpty()) {
+            // The strenght is always set back to 0
               strength = 0;
+              // the black position is selected from the stack of black positions
               blackPosition = (Square) bStack.pop();
+              // if the move selected is going to land on a black piece then the if statement occurs
               if ((presentMove.getLanding().getXC() == blackPosition.getXC()) && (presentMove.getLanding().getYC() == blackPosition.getYC())) {
 
-                  //Assign strength to pieces
+                  // Different values are assigned to the pieces as per keith's tutorial, if none of them are the piece in the position, then it sets the value to 10 which must be the king
                   if (blackPosition.getName().equals("BlackQueen")) {
                       strength = 9;
                   } else if (blackPosition.getName().equals("BlackRook")) {
@@ -59,17 +72,19 @@ public class AIAgent{
                       strength = 10;
                   }
               }
-              //updating the best move
+              // a move is initially chosen and the strenght is set to the chosenPieceStrenght, this chosenPieceStrenght is not set back to 0 at the start of the loop
+              // if the next move selected has a higher strenght then that move is set to the new chosenPieceStrenght and that move is set to the bestmove
+              // this loops until every possibilty is chosen, in the end the piece with the highest strenght will be taken
               if (strength > chosenPieceStrength) {
                   chosenPieceStrength = strength;
                   bestMove = presentMove;
               }
           }
-          //reloading the black squares
+          // the black postion stack needs to be reloaded as we've removed items from it in the second loop
           bStack = (Stack) blackPossibilitiesStack.clone();
       }
 
-      // Make the best move if not available make a random move.
+      // if chosen pieces remains 0 then a random move is selected
       if (chosenPieceStrength > 0) {
           System.out.println("Selected AI Agent - Next best move: " +chosenPieceStrength);
           return bestMove;
@@ -79,6 +94,8 @@ public class AIAgent{
 
   }
   public Move twoLevelsDeep(Stack possibilities){
+    //loop through the black posibilities to figure out what would be considered the worst move for white to makes
+    //could use similar algorithm to the above table but use negative values for the 'bad' moves
     Move selectedMove = new Move();
     return selectedMove;
   }
